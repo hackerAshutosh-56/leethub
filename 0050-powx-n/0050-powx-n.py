07c31e83239cdd3cbe99f538d371bc1a6b26cbbf
Ashutosh Dixit <2k24.csai1a.2410616@gmail.com>
2026-08-07 16:33:07 +0530

    Time: 0 ms (100%), Space: 19.4 MB (86.33%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -3,6 +3,8 @@ class Solution:
         def power (x,n):
             if n==0:
                 return 1
+            if n==1:
+                return x    
             half=power(x,n//2)
             if n%2==0:
                 return half*half 
